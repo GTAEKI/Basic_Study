bacaa363b7de03f3ab75360c0dcc95d44dad277a
BaeKyungTaek <kyungtaek0204@gmail.com>
2023-12-05 20:47:54 +0900

    [코테] 문제 테스트

---
+++ CodingTest_Practice/11655/11655.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string context;
+
+int main()
+{
+    getline(cin,context);
+    
+    for(auto i : context)
+    {
+        if(i >= 'a' && i <= 'z')
+        {
+            cout << 'a' + ((i - 'a' + 13) % 26);
+        }
+        else if(i >= 'A' && i <= 'Z')
+        {
+            cout << 'A' + ((i - 'A' + 13) % 26);
+        }
+        else
+        {
+            cout << i;
+        }
+    }
+    return 0;
+}
