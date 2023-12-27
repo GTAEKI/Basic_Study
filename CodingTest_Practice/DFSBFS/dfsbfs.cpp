e6f3ecc30de5e007e61729ec84fe89209d029e13
BaeKyungTaek <kyungtaek0204@gmail.com>
2023-12-27 22:17:45 +0900

    [코테] DFS, BFS 맛보기

---
+++ CodingTest_Practice/DFSBFS/dfsbfs.cpp
@@ -0,0 +1,25 @@
+dfs(int here)
+{
+    if(visited[here]) return;
+    visited[here] = 1;
+    for(int there : adj[here])
+    {
+        dfs(there);
+    }
+}
+
+bfs(int here)
+{
+    queue<int> q;
+    visited[here] = 1;
+    q.push(here);
+    while(q.size())
+    {
+        int here = q.front(); q.pop();
+        for(int there : adj[here])
+        {
+            visited[there] = visited[here]+1;
+            q.push(there);
+        }
+    }
+}
