e7b1bfa064f940122769996da806fd996596e6c8
BaeKyungTaek <kyungtaek0204@gmail.com>
2024-01-07 20:36:34 +0900

    [운영체제] 프로세스 ID와 스레드 ID가 다른 것 확인하기

--- Basic/Thread/Thread.cpp
+++ Basic/Thread/Thread.cpp
@@ -1,8 +1,20 @@
-#include<stdio.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+
+void * foo(void *arg)
+{
+    long thread_id = (long int) pthread_self();
+    printf("process id is %d\n", getpid());
+    printf("this is thread %ld\n", thread_id);
+    
+    return NULL;
+}
 
 int main()
 {
-    printf("process id is %d", getpid());
+    pthread_t thread1;
+    pthread_create(&thread1, NULL,foo, NULL);
+    pthread_join(thread1, NULL);
     return 0;
 }
