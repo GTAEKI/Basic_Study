10c6878fb34f600be0df0bb57669a3a4278bad10
BaeKyungTaek <kyungtaek0204@gmail.com>
2024-01-07 20:41:06 +0900

    [운영체제] 한 프로세스 내에서 여러개의 스레드 실행

--- Basic/Thread/Thread.cpp
+++ Basic/Thread/Thread.cpp
@@ -4,6 +4,32 @@
 
 void * foo(void *arg)
 {
+    printf("foo executed\n");
+
+    long thread_id = (long int) pthread_self();
+    printf("process id is %d\n", getpid());
+    printf("this is thread %ld\n", thread_id);
+    
+    return NULL;
+}
+
+void * bar(void *arg)
+{
+    printf("bar executed\n");
+
+
+    long thread_id = (long int) pthread_self();
+    printf("process id is %d\n", getpid());
+    printf("this is thread %ld\n", thread_id);
+    
+    return NULL;
+}
+
+void * baz(void *arg)
+{
+    printf("baz executed\n");
+
+
     long thread_id = (long int) pthread_self();
     printf("process id is %d\n", getpid());
     printf("this is thread %ld\n", thread_id);
@@ -18,8 +44,8 @@ int main()
     pthread_t thread3;
 
     pthread_create(&thread1, NULL,foo, NULL);
-    pthread_create(&thread2, NULL,foo, NULL);
-    pthread_create(&thread3, NULL,foo, NULL);
+    pthread_create(&thread2, NULL,bar, NULL);
+    pthread_create(&thread3, NULL,baz, NULL);
 
     pthread_join(thread1, NULL);
     pthread_join(thread2, NULL);
