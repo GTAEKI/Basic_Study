4241a09d386305624b6b10415673f12887f5e6b5
BaeKyungTaek <kyungtaek0204@gmail.com>
2024-01-07 20:38:00 +0900

    [운영체제] 같은 프로세스를 공유하는 다른 쓰레드 ID값으로 확인해보기

--- Basic/Thread/Thread.cpp
+++ Basic/Thread/Thread.cpp
@@ -14,7 +14,16 @@ void * foo(void *arg)
 int main()
 {
     pthread_t thread1;
+    pthread_t thread2;
+    pthread_t thread3;
+
     pthread_create(&thread1, NULL,foo, NULL);
+    pthread_create(&thread2, NULL,foo, NULL);
+    pthread_create(&thread3, NULL,foo, NULL);
+
     pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
+    pthread_join(thread3, NULL);
+
     return 0;
 }
