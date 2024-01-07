c4a30f0208c9e134da8080fef4c3acb30d7a49d0
BaeKyungTaek <kyungtaek0204@gmail.com>
2024-01-07 20:48:03 +0900

    [운영체제] mutex lock 사용해보기, 임계구역을 컨트롤하여 정확한 값을 도출

--- Basic/MutexLock/MutexLock.cpp
+++ Basic/MutexLock/MutexLock.cpp
@@ -2,25 +2,34 @@
 #include <pthread.h>
 #define NUM_THREADS 4
 
+// mutex라는 이름의 자물쇠 초기화
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
 int shared = 0;
 
 void *foo(void * arg)
 {
+    pthread_mutex_lock(&mutex); // 자물쇠 잠그기
     for (int i = 0; i < 10000; ++i) {
         shared += 1;
     }
+    pthread_mutex_unlock(&mutex); // 자물쇠 풀기
     return NULL;
 }
 
 int main()
 {
     pthread_t threads[NUM_THREADS];
+
     for (int i = 0; i < NUM_THREADS; ++i) {
         pthread_create(&threads[i], NULL, foo, NULL);
     }
+
     for (int i = 0; i < NUM_THREADS; ++i) {
         pthread_join(threads[i], NULL);
     }
+
     printf("final result is %d\n", shared);
+
     return 0;
 }
