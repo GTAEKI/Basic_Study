bb8d0e65ef3bb49fbd240d091c35c1cb0418731e
DESKTOP-02RE0NO\Kyungtaek <kyungtaek0204@gmail.com>
2024-04-08 19:20:10 +0900

    [Add] 맵 제작, 기본 세팅

---
+++ AlgorithmPractice/Algorithm/Maze/ConsoleHelper.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include <windows.h>
+#include "Types.h"
+
+enum class ConsoleColor
+{
+	// 실질적으로 나중에 사용할 API와 연관이 있다.
+	// 콘솔에 사용할 색
+	BLACK = 0,
+	RED = FOREGROUND_RED,
+	GREEN = FOREGROUND_GREEN,
+	BLUE = FOREGROUND_BLUE,
+	YELLOW = RED | GREEN,
+	WHITE = RED | GREEN | BLUE,
+};
+
+class ConsoleHelper
+{
+public:
+	static void SetCursorPosition(int32 x, int32 y); // 커서 설정
+	static void SetCursorColor(ConsoleColor color); // 커서 색상 결정
+	static void ShowConsoleCursor(bool flag); // 커서 보여줄지 안보여줄지
+};
+
---
+++ AlgorithmPractice/Algorithm/Maze/Maze.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include "pch.h"
+#include "ConsoleHelper.h"
+
+int main()
+{
+	std::locale::global(std::locale("ko_KR.UTF-8"));
+
+	// 거의 대부분의 게임들은 무한루프안에서 돌아간다.
+	// 물론 온라인게임일 경우 일부 로직은 서버쪽에 분산이 되어 처리될것
+	// 입력
+	// 로직
+	// 랜더링
+	uint64 lastTick = 0;
+
+	while (true)
+	{
+#pragma region 프레임 관리
+		// 기본적인 프레임 관리
+		// 고정프레임, 가변프레임이 있다
+		// 고정은 1초동안 고정적인 프레임만 허락하겠다 (30, 60 등)
+		// 요즘 게임들은 컴퓨터 좋은거에 맞춰 그냥 200 300 까지도 가는것을 볼 수 있는데
+		// 우리는 그냥 가변으로
+		const uint64 currentTick = ::GetTickCount64();
+		const uint64 deltaTick = currentTick - lastTick;
+		lastTick = currentTick;
+#pragma endregion
+
+		ConsoleHelper::SetCursorPosition(0, 0);
+		ConsoleHelper::ShowConsoleCursor(false);
+		ConsoleHelper::SetCursorColor(ConsoleColor::RED);
+
+		const char* TILE = "■";
+
+		for (int32 y = 0; y < 25; y++)
+		{
+			for (int32 x = 0; x < 25; x++)
+			{
+				cout << TILE;
+			}
+
+			cout << endl;
+		}
+	}
+}
