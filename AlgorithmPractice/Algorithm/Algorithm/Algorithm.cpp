cbb118553990f3ed12ac1a8f1858793c4d2d81b5
DESKTOP-02RE0NO\Kyungtaek <kyungtaek0204@gmail.com>
2024-04-09 18:28:52 +0900

    [스택,큐] 스택 큐 구현연습

--- AlgorithmPractice/Algorithm/Algorithm/Algorithm.cpp
+++ AlgorithmPractice/Algorithm/Algorithm/Algorithm.cpp
@@ -1,217 +1,145 @@
 ﻿#include<iostream>
+#include<vector>
 #include<list>
+#include<stack>
+#include<queue>
 #include "LinkedList.h"
 using namespace std;
-//
-//template<typename T>
-//class Node
-//{
-//public:
-//	Node() : _prev(nullptr), _next(nullptr), _data(T())
-//	{
-//
-//	}
-//
-//	Node(const T& value) : _prev(nullptr), _next(nullptr), _data(value)
-//	{
-//
-//	}
-//
-//public:
-//	Node* _prev;
-//	Node* _next;
-//	T	  _data;
-//};
-//
-//template<typename T>
-//class Iterator 
-//{
-//public:
-//	Iterator() :_node(nullptr) 
-//	{
-//	
-//	}
-//
-//	Iterator(Node<T>* node) : _node(node) 
-//	{
-//	
-//	}
-//
-//	// ++it
-//	Iterator& operator++() 
-//	{
-//		_node = _node->_next;
-//		return *this;
-//	}
-//
-//	// it++
-//	Iterator operator++(int) 
-//	{
-//		Iterator<T> temp = *this;
-//		_node = _node->_next;
-//		return temp;
-//	}
-//
-//	// --it
-//	Iterator& operator--()
-//	{
-//		_node = _node->_prev;
-//		return *this;
-//	}
-//
-//	// it--
-//	Iterator operator--(int)
-//	{
-//		Iterator<T> temp = *this;
-//		_node = _node->_prev;
-//		return temp;
-//	}
-//
-//	// *it
-//	T& operator*() 
-//	{
-//		return _node->_data;
-//	}
-//
-//	bool operator==(const Iterator& other) 
-//	{
-//		return _node == other._node;
-//	}
-//
-//	bool operator != (const Iterator& other) 
-//	{
-//		return _node != other._node;
-//	}
-//
-//public:
-//	Node<T>* _node;
-//};
-//
-//template<typename T>
-//class List
-//{
-//public:
-//	List() : _size(0) 
-//	{
-//		// [head] <-> ... <-> [tail]
-//		_head = new Node<T>();
-//		_tail = new Node<T>();
-//		_head->_next = _tail;
-//		_tail->_prev = _head;
-//	}
-//
-//	~List() 
-//	{
-//		while (_size > 0)
-//			pop_back();
-//
-//		delete _head;
-//		delete _tail;
-//	}
-//
-//	void push_back(const T& value) 
-//	{
-//		AddNode(_tail, value);
-//	}
-//
-//	void pop_back() 
-//	{
-//		RemoveNode(_tail->_prev);
-//	}
-//
-//	int size() 
-//	{
-//		return _size;
-//	}
-//
-//private:
-//	Node<T>* AddNode(Node<T>* before, const T& value) 
-//	{
-//		Node<T>* newNode = new Node<T>(value);
-//		Node<T>* prevNode = before->_prev;
-//
-//		prevNode->_next = newNode;
-//		newNode->_prev = prevNode;
-//
-//		newNode->_next = before;
-//		before->_prev = newNode;
-//
-//		_size++;
-//
-//		return newNode;
-//	}
-//
-//	Node<T>* RemoveNode(Node<T>* node) 
-//	{
-//		Node<T>* prevNode = node->_prev;
-//		Node<T>* nextNode = node->_next;
-//
-//		prevNode->_next = nextNode;
-//		nextNode->_prev = prevNode;
-//
-//		delete node;
-//
-//		_size--;
-//
-//		return nextNode;
-//	}
-//
-//public:
-//	using iterator = Iterator<T>;
-//
-//	iterator begin() { return iterator(_head->_next); }
-//	iterator end() { return iterator(_tail); }
-//
-//	// it '앞에(이전)' 추가
-//	iterator insert(iterator it, const T& value) 
-//	{
-//		Node<T>* node = AddNode(it._node, value);
-//		return iterator(node);
-//	}
-//
-//	iterator erase(iterator it) 
-//	{
-//		Node<T>* node = RemoveNode(it._node);
-//		return iterator(node);
-//	}
-//
-//private:
-//
-//	// 만약 널포인터가 될 수 있다고 하면, 계속 널 체크를 해줘야 하는문제가 있음
-//	// 그래서 실제 사용하느 데이터만가리키ㅔㄱ 하는게아니라 더미노드를 만들어 아무리 데이터가 없어도 시작상태로 노드를 두개를 만든다
-//	Node<T>* _head;
-//	Node<T>* _tail;
-//	int	     _size;
-//
-//};
+
+
+template<typename T>
+class ArrayQueue
+{
+public:
+    ArrayQueue() :_front(0), _back(0), _size(0)
+    {
+        _container.resize(100);
+    }
+
+    void push(const T& value)
+    {
+        // TODO:다 찼는지 체크
+        if (_size == _container.size()) 
+        {
+            int newSize = max(1, _size * 2);
+            vector<T> newData;
+            newData.resize(newSize);
+
+            // 데이터 복사
+            for (int i = 0; i < _size; i++) 
+            {
+                int index = (_front + i) % _container.size();
+                newData[i] = _container[index];
+            }
+
+            _container.swap(newData);
+            _front = 0;
+            _back = _size;
+        }
+        
+        _container[_back] = value;
+        _back = (_back + 1) % _container.size();
+        _size++;
+    }
+
+    void pop()
+    {
+        // TODO: 비어있는지 체크
+        _front = (_front + 1) % _container.size();
+        _size--;
+    }
+
+    T& front()
+    {
+        // TODO : 데이터가 있는지 체크
+        return _container[_front];
+    }
+
+    bool empty()
+    {
+        return _size == 0;
+    }
+
+    int size()
+    {
+        return _size;
+    }
+
+private:
+    vector<T> _container;
+
+    int _front = 0;
+    int _back = 0;
+    int _size = 0;
+};
+
+
+template<typename T>
+class ListQueue
+{
+public:
+    void push(const T& value) 
+    {
+        _container.push_back(value);
+    }
+
+    void pop() 
+    {
+        // erase를 함수로 지원한다고 해도 빠르게 동작한다는 보장이 없다.
+        //_container.erase(_container.begin()); 
+        
+        // 동적배열 보다는 연결리스트로 만드는게 일단은 더 간단해 보임
+        _container.pop_front();
+
+        /*vector<T> temp;
+        temp.reserve(_container.size() - 1);
+        for (int i = 1; i < _container.size(); i++) 
+        {
+            temp.push_back(_container[i]);
+        }
+        _container.clear();
+        _container = temp;*/
+    }
+
+    T& front() 
+    {
+        return _container.front();
+    }
+
+    bool empty() 
+    {
+        return _container.empty();
+    }
+
+    int size() 
+    {
+        return _container.size();
+    }
+
+private:
+    deque<T> _container;
+    //list<T> _container;
+    //vector<T> _container;
+};
+
 
 int main()
 {
-    LinkedList<int> li;
+    //ListQueue<int> q;
 
-    LinkedList<int>::iterator eraseIt = li.end(); // 초기화
+    ArrayQueue<int> q; // stack 및 queue는 deque를 기본 컨테이너로 활용을 한다.
 
-    for (int i = 0; i < 10; i++)
+    for (int i = 0; i < 100; i++) q.push(i);
+
+    while (q.empty() == false) 
     {
-        if (i == 5)
-        {
-            eraseIt = li.insert(li.end(), i);
-        }
-        else
-        {
-            //li.push_back(i);
-            li.push_front(i);
-        }
+        int value = q.front();
+        q.pop();
+        cout << value << endl;
     }
 
-    li.pop_back();
-    
-    //li.erase(eraseIt);
-
-    for (LinkedList<int>::iterator it = li.begin(); it != li.end(); it++)
-    {
-        cout << (*it) << endl;
-    }
+    int size = q.size();
+    cout << size << endl;
 
     return 0;
 }
