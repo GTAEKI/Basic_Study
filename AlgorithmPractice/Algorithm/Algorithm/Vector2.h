0d42337cad0d55297b79a6a87fb3134f8dfca3b3
DESKTOP-02RE0NO\Kyungtaek <kyungtaek0204@gmail.com>
2024-04-21 14:07:43 +0900

    [Vector] 벡터 재구현4

--- AlgorithmPractice/Algorithm/Algorithm/Vector2.h
+++ AlgorithmPractice/Algorithm/Algorithm/Vector2.h
@@ -4,22 +4,44 @@ template<typename T>
 class Vector 
 {
 public:
-	Vector() : _capacity(0), _size(0), _data(nullptr)
+	Vector():_size(0),_capacity(0),_data(nullptr)
 	{
-		
+	
 	}
 
-	~Vector()
+	~Vector() 
 	{
-		if(_data)
+		if (_data) 
+		{
 			delete[] _data;
+		}
+	}
+
+	void reserve(const int& capacity) 
+	{
+		if (_capacity >= capacity) return;
+
+		_capacity = capacity;
+		
+		T* newData = new T[_capacity];
+
+		for (int i = 0; i < _size; i++) 
+		{
+			newData[i] = data[i];
+		}
+
+		if (_data) 
+		{
+			delete[] _data;
+		}
+		_data = newData;
 	}
 
 	void push_back(const T& value) 
 	{
 		if (_size >= _capacity) 
 		{
-			int newCapacity = static_cast<int>(_capacity * 1.5);
+			int newCapacity = _capacity * 1.5;
 			if (newCapacity <= 1) newCapacity++;
 
 			reserve(newCapacity);
@@ -31,66 +53,38 @@ public:
 
 	void pop_back() 
 	{
-		if (size > 0)
+		if (_size > 0)
 			_size--;
 	}
 
 	void empty() 
 	{
-		return _size == 0;
-	}
-
-	int size() 
-	{
-		return _size;
-	}
-
-	int capacity() 
-	{
-		return _capacity;
-	}
-
-	void reserve(const int& capacity) 
-	{
-		if (capacity <= _capacity)
-			return;
-		_capacity = capacity;
-
-		T* newData = new T[_capacity];
-
-		for (int i = 0; i < _size; i++)
-		{
-			newData[i] = _data[i];
-		}
-
-		if (_data)
-		{
-			delete[] _data;
-		}
-		_data = newData;
+		return _size == 0;=
 	}
 
 	void clear() 
 	{
-		if (_data)
+		if (_data) 
 		{
 			delete[] _data;
-			_data = new T(_capacity);
+			_data = new T[_capacity];
 		}
 
 		_size = 0;
 	}
 
-	T& operator [](const int& index)
+	void size() 
 	{
-		return _data[index];
+		return _size;
 	}
 
-
-
+	void capacity() 
+	{
+		return _capacity;
+	}
 
 private:
-	T* _data;
 	int _size;
 	int _capacity;
+	T* _data;
 };
