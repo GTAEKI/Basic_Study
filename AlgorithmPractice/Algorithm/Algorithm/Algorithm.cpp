5a9fda45b2c809358417bd8750bc52647ce80547
DESKTOP-02RE0NO\Kyungtaek <kyungtaek0204@gmail.com>
2024-04-22 17:15:00 +0900

    [LinkedList] 재구현

--- AlgorithmPractice/Algorithm/Algorithm/Algorithm.cpp
+++ AlgorithmPractice/Algorithm/Algorithm/Algorithm.cpp
@@ -7,45 +7,64 @@
 #include<Windows.h>
 using namespace std;
 
-#include"Vector3.h"
+#include"LinkedList3.h"
 
 int main()
 {
-	Vector<int> v3;
+#pragma region MyRegion
 
-	v3.reserve(8);
+	//Vector<int> v3;
 
-	cout << v3.size() << v3.capacity() << endl;
+	//v3.reserve(8);
 
-	v3.push_back(1);
-	v3.push_back(2);
-	v3.push_back(3);
-	v3.push_back(4);
-	v3.push_back(5);
-	v3.push_back(6);
-	v3.push_back(7);
-	v3.push_back(8);
+	//cout << v3.size() << v3.capacity() << endl;
 
-	for (int i = 0; i < v3.size(); i++) 
+	//v3.push_back(1);
+	//v3.push_back(2);
+	//v3.push_back(3);
+	//v3.push_back(4);
+	//v3.push_back(5);
+	//v3.push_back(6);
+	//v3.push_back(7);
+	//v3.push_back(8);
+
+	//for (int i = 0; i < v3.size(); i++) 
+	//{
+	//	cout << v3[i] << endl;
+	//}
+
+	//cout << v3.size() << v3.capacity() << endl;
+
+	//v3.push_back(9);
+
+	//cout << v3.size() << v3.capacity() << endl;
+
+	//v3.pop_back();
+	//v3.pop_back();
+	//v3.pop_back();
+
+	//cout << v3.size() << v3.capacity() << endl;
+
+	//v3.clear();
+
+	//cout << v3.size() << v3.capacity() << endl;
+#pragma endregion
+
+	LinkedList<int> l3;
+
+	l3.push_back(3);
+	l3.push_back(4);
+	l3.push_back(5);
+	l3.push_back(6);
+
+	for (auto iter = l3.begin(); iter != l3.end(); iter++) 
 	{
-		cout << v3[i] << endl;
+		cout << *iter << endl;
 	}
 
-	cout << v3.size() << v3.capacity() << endl;
+	l3.pop_back();
 
-	v3.push_back(9);
-
-	cout << v3.size() << v3.capacity() << endl;
-
-	v3.pop_back();
-	v3.pop_back();
-	v3.pop_back();
-
-	cout << v3.size() << v3.capacity() << endl;
-
-	v3.clear();
-
-	cout << v3.size() << v3.capacity() << endl;
+	cout << l3.size();
 
 	return 0;
 }
